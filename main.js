6d4841c54e0629772594775a166fae0aaca3d135
estiBarel <esti.barel@wibbitz.com>
2023-01-12 08:55:08 +0200

    V2

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-
 import data from "./pokedex.json" assert { type:"json" };
 
 console.log(data)
 console.log(typeof(data))
+console.log(data[0]['description']);
 
 
-for (let i=0; i<13; i++) {
-    //creating small div
+
+for (let i=0; i<12; i++) {
+    //creating small div for each pokemon
     const smallContainer = document.createElement("div");
     smallContainer.className = "one-pokemon-div";
+    smallContainer.setAttribute("id",data[i]['id']);
+
 
     //adding id -> small div//
-    const pokemonId = document.createElement("p"); 
+    const pokemonId = document.createElement("div"); 
     pokemonId.innerHTML = `#00${data[i]['id']}`;
     smallContainer.appendChild(pokemonId);
     pokemonId.className = "type-of-id";
@@ -22,7 +25,7 @@ for (let i=0; i<13; i++) {
     smallContainer.appendChild(pokemonImg);
     
     //adding name -> small div//
-    const pokemonName = document.createElement("p");
+    const pokemonName = document.createElement("div");
     pokemonName.innerHTML = `${data[i]['name']['english']}`;
     smallContainer.appendChild(pokemonName);
     pokemonName.className = "type-of-name";
@@ -34,42 +37,68 @@ for (let i=0; i<13; i++) {
 
 
 
-// let country = {
-//     id: "312232481", 
-//     name: "israel",
-//     population: 7000,
-// };
+function x() {
+    for (let i=1; i<13; i++) {
+                
+        const BigDivForAllModals = document.getElementById("all-modal");
+        // The Modal
+        const modal= document.createElement("div");
+        modal.setAttribute("id",`myModal-${i}`);
+        modal.className = "modal";
+        console.log("modal::::" , modal);
+        
+        // Modal content 
+        const modalContent = document.createElement("div");
+        modalContent.className = "modal-content";
+        console.log("modalContent::::" , modalContent);
+        
+        // create p for modal text
+        const modalPara = document.createElement("p");
+        const detailsForPokemon = {
+            description: data[i]['description'], 
+            HP: data[i]['HP'],
+            Attack: data[i]['Attack'],
+            }
+        //     Defense:
+        //     Spaical Atk:
+        //     Spaical Def:
+        //     Speed:
+        //     Total:
+        // },
+    //      {
+        //     id:
+        //     img:
+        //     name:
+        //     types:
+        // }
+        const myArray = Object.values(detailsForPokemon);
+        modalPara.innerHTML = myArray ;
+        console.log("modalPara::::::" , modalPara);
+        modalContent.appendChild(modalPara);
+        
+        // create span for modal close
+        const modalSpan = document.createElement("span");
+        modalSpan.className = "close";
 
-// let countryJason = JSON.stringify(country);
+        modalContent.appendChild(modalSpan);
+        
+        // append all child to modal
+        modal.appendChild(modalContent);
+       
+        //adding the modal div to original pokemon div
+        BigDivForAllModals.appendChild(modal);    
+        
+    }}
+    
+    x()
 
-// console.log(country);
-// console.log(countryJason);
-// console.log(typeof(countryJason));
+    for (let i=1; i<13;i++) {
+    const firstpokemon = document.getElementById(i);
+    // const firstModal = document.getElementById("myModal-1")
 
-// let yabeshet = {
-//     continentName: "eur",
-//     countries: [
-//     {
-//         id: "20097675", 
-//         name: "egypth",
-//         population: 1000,
-//     },
-//     {
-//         id: "00000", 
-//         name: "spain",
-//         population: 8000,
-//     },
-//     {
-//         id: "99999", 
-//         name: "cyprus",
-//         population: 2000,
-//     }]
+    firstpokemon.addEventListener("click", function() {
+        document.getElementById(`myModal-${i}`).style.display = "block";
+    });
+}
 
-// };
-
-// let yabeshetJason = JSON.stringify(yabeshet);
-// let objFromJsonYabeshet = JSON.parse(yabeshetJason);
-
-// console.log(yabeshet);
-// console.log(yabeshetJason);
-// console.log(objFromJsonYabeshet );
+// ${i}
